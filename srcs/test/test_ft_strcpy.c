b4094ba09030ad1d712957101c1fdd4012f9cb89
caio-vinicius <contato@caiovinicius.dev.br>
2021-04-04 20:39:58 +0200

    adjust norme in ft_strcpy

--- srcs/test/test_ft_strcpy.c
+++ srcs/test/test_ft_strcpy.c
@@ -16,26 +16,25 @@
 void	test_ft_strcpy(void)
 {
 	char *str;
-	char array1[20] = "1 2 3 4";
-	char array2[20] = "a b c d";
+	char array1[20];
+	char array2[20];
 
+	array1 = "1 2 3 4";
+	array2 = "a b c d";
 	str = malloc(4 + 1);
 	printf(" == FT_STRCPY == \n");
 	printf("BEFORE1: %p\n", str);
 	str = strcpy(str, "caio");
 	printf("   strcpy(str, 'caio'): %s | %p\n", str, str);
-	str = malloc(4 + 1);	
+	str = malloc(4 + 1);
 	printf("BEFORE2: %p\n", str);
 	str = ft_strcpy(str, "caio");
 	printf("ft_strcpy(str, 'caio'): %s | %p\n", str, str);
-	
 	printf(" == FT_STRCPY 2 == \n");
-	
 	printf("BEFORE2: %p\n", array1);
 	str = ft_strcpy(array1, array2);
 	printf("ft_strcpy(array1, array2): %s | %p\n", str, str);
-	
 	printf("BEFORE1: %p\n", array1);
 	str = strcpy(array1, array2);
-	printf("   strcpy(array1, array2): %s | %p\n", str, str);	
+	printf("   strcpy(array1, array2): %s | %p\n", str, str);
 }
