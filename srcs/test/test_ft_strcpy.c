68a0314d25e569ff37bd817950c747087a366ca0
caio-vinicius <contato@caiovinicius.dev.br>
2021-04-04 20:57:23 +0200

    remove second test from strcpy

--- srcs/test/test_ft_strcpy.c
+++ srcs/test/test_ft_strcpy.c
@@ -16,11 +16,7 @@
 void	test_ft_strcpy(void)
 {
 	char *str;
-	char array1[20];
-	char array2[20];
 
-	array1 = "1 2 3 4";
-	array2 = "a b c d";
 	str = malloc(4 + 1);
 	printf(" == FT_STRCPY == \n");
 	printf("BEFORE1: %p\n", str);
@@ -30,11 +26,4 @@ void	test_ft_strcpy(void)
 	printf("BEFORE2: %p\n", str);
 	str = ft_strcpy(str, "caio");
 	printf("ft_strcpy(str, 'caio'): %s | %p\n", str, str);
-	printf(" == FT_STRCPY 2 == \n");
-	printf("BEFORE2: %p\n", array1);
-	str = ft_strcpy(array1, array2);
-	printf("ft_strcpy(array1, array2): %s | %p\n", str, str);
-	printf("BEFORE1: %p\n", array1);
-	str = strcpy(array1, array2);
-	printf("   strcpy(array1, array2): %s | %p\n", str, str);
 }
